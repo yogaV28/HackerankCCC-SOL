30dc517aa55c107016ef8511a3009b8ed05f34d9
yogaV28 <121656366+yogaV28@users.noreply.github.com>
2023-07-08 14:38:55 +0530

    Create P 303 - A Sorted Team.c

---
+++ P 303 - A Sorted Team.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    scanf("%d",&n);
+    int players[n], i, j, index;
+    for(i=0;i<n;i++)
+        scanf("%d",&players[i]);
+    for(i=0;i<n-1;i++)
+    {
+        index = i;
+        for(j=i+1;j<n;j++)
+        {
+            if(players[j] < players[index])
+                index = j;
+        }
+        if(index != i)
+        {
+            int temp = players[index];
+            players[index] = players[i];
+            players[i] = temp;
+        }
+    }
+    for(i=0;i<n;i++)
+        printf("%d ",players[i]);
+    return 0;
+}
